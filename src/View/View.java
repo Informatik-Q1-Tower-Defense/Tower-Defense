6cb290022850039ca515e6d6eae29b23881eb4bf
Tim Jaeger <timjae@schuldorf.de>
2020-09-06 19:37:58 +0200

    View.java als Grundlage für die GUI

--- src/View/View.java
+++ src/View/View.java
@@ -1,8 +1,29 @@
 package View;
 
 import javafx.scene.Node;
+import Model.Position;
 
-public interface View {
+public abstract class View {
 
-    public Node[] getElements();
+    public Position center;
+
+    public View(Position center) {
+
+        this.center = center;
+    }
+
+    abstract protected Node[] getElements();
+
+    public final Node[] render() {
+
+        Node[] elements = this.getElements();
+
+        for(Node element: elements) {
+
+            element.setLayoutX(element.getLayoutX() + this.center.x);
+            element.setLayoutY(element.getLayoutY() + this.center.y);
+        }
+
+        return elements;
+    }
 }
