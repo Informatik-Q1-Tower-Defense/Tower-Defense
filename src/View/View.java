9a0f60c9e6ce8fe009e751f06a186821412a50c6
Tim Jaeger <timjae@schuldorf.de>
2020-09-06 22:19:11 +0200

    Änderungen an View.java

--- src/View/View.java
+++ src/View/View.java
@@ -6,24 +6,8 @@ import Model.Position;
 public abstract class View {
 
     public Position center;
+    private int width, height;
 
-    public View(Position center) {
-
-        this.center = center;
-    }
-
-    abstract protected Node[] getElements();
-
-    public final Node[] render() {
-
-        Node[] elements = this.getElements();
-
-        for(Node element: elements) {
-
-            element.setLayoutX(element.getLayoutX() + this.center.x);
-            element.setLayoutY(element.getLayoutY() + this.center.y);
-        }
-
-        return elements;
-    }
+    private Node[] content;
+    private View[] views;
 }
