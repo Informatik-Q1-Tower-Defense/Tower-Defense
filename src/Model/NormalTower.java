c1e21d86774e9cd60c6b42a88594655c4dac8505
Riemann01 <62616772+Riemann01@users.noreply.github.com>
2020-09-11 15:16:08 +0200

    Update NormalTower.java

--- src/Model/NormalTower.java
+++ src/Model/NormalTower.java
@@ -1,1 +1,33 @@
+package Model;
 
+import Controller.ViewController;
+
+public class FreezeTower extends Tower {
+  
+  private int freezePoints;
+  
+  public FreezeTower(Position pos){
+
+    super(pos);
+    
+    super.setPictureSource("");
+    super.setCost(150);
+    
+    this.freezePoints = 10;
+  }
+  
+  public void shoot(Wave wave){
+    Enemy[] enemies = wave.enemies;
+
+    Position[] way = ViewController.mapData.way;
+
+    for (int i = 0; i < enemies.length; i++) {
+      double eRange = Math.sqrt(Math.abs((way[enemies[i].getPositionIndex()].x-super.getPosition().x)*(way[enemies[i].getPositionIndex()].x-super.getPosition().x) + (way[enemies[i].getPositionIndex()].y-super.getPosition().y)*(way[enemies[i].getPositionIndex()].y-super.getPosition().y))) ;
+      if (super.getRange() > eRange) {
+        enemies[i].hp -= super.getDamage();
+        break;
+      } // end of if
+    }
+  }
+  
+}
